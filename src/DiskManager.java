364852354024eea3cb6e1691f14ae2ba6e00b15c
nbetry <nbetry@macalester.edu>
2025-04-16 10:41:03 -0500

    finished basic gameboard

---
+++ src/DiskManager.java
@@ -0,0 +1,13 @@
+import edu.macalester.graphics.CanvasWindow;
+
+public class DiskManager {
+    private CanvasWindow canvas;
+
+    public DiskManager(CanvasWindow canvas){
+        this.canvas = canvas;
+    }
+
+    public void addToBoard(Disk disk){
+        canvas.add(disk.getRectangle());
+    }
+}
